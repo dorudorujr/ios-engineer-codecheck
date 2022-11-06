2c602e22601e569be100df23466f69c30df1bb73
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-06 13:55:39 +0900

    リポジトリ詳細画面のスターの数を表示しているLabelの変数名をRename

--- iOSEngineerCodeCheck/RepositoryDetailViewController.swift
+++ iOSEngineerCodeCheck/RepositoryDetailViewController.swift
@@ -15,7 +15,7 @@ class RepositoryDetailViewController: UIViewController {
     
     @IBOutlet weak var languageLabel: UILabel!
     
-    @IBOutlet weak var StrsLbl: UILabel!
+    @IBOutlet weak var stargazersCountLabel: UILabel!
     @IBOutlet weak var WchsLbl: UILabel!
     @IBOutlet weak var FrksLbl: UILabel!
     @IBOutlet weak var IsssLbl: UILabel!
@@ -28,7 +28,7 @@ class RepositoryDetailViewController: UIViewController {
         let repo = repositoryListViewController.repositoryDataList[repositoryListViewController.repositoryListIndex]
         
         languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
-        StrsLbl.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        stargazersCountLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
         WchsLbl.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
         FrksLbl.text = "\(repo["forks_count"] as? Int ?? 0) forks"
         IsssLbl.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
