260862eccaf711052d702831c021540f6486adc2
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-06 12:32:57 +0900

    ViewControllerのunneeded_parentheses_in_closure_argumentとunused_closure_parameter

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
         
         if !word.isEmpty {
             url = "https://api.github.com/search/repositories?q=\(word!)"
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+            task = URLSession.shared.dataTask(with: URL(string: url)!) { data, _, _ in
                 if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     if let items = obj["items"] as? [[String: Any]] {
                     self.repo = items
