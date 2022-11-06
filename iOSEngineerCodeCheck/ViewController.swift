3dadf02dae25918a40550b13513dee4668aa458e
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-06 12:31:29 +0900

    ViewControllerのoperator_usage_whitespaceを修正

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
 
     @IBOutlet weak var SchBr: UISearchBar!
     
-    var repo: [[String: Any]]=[]
+    var repo: [[String: Any]] = []
     
     var task: URLSessionTask?
     var word: String!
