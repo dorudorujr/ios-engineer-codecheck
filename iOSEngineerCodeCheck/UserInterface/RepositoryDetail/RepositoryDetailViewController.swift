d86c57f888e7ee46f806f2b292c915c97ea7818c
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-20 23:32:44 +0900

    VC初期化時に値を与えていることが保証されるので強制アンラップに修正

--- iOSEngineerCodeCheck/UserInterface/RepositoryDetail/RepositoryDetailViewController.swift
+++ iOSEngineerCodeCheck/UserInterface/RepositoryDetail/RepositoryDetailViewController.swift
@@ -21,26 +21,22 @@ class RepositoryDetailViewController: UIViewController {
     @IBOutlet weak private var forksCountLabel: UILabel!
     @IBOutlet weak private var openIssuesCountLabel: UILabel!
     
-    var repositoryData: GitHubRepositoryData?
+    var repositoryData: GitHubRepositoryData!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = repositoryData?.fullName ?? ""
-        languageLabel.text = "Written in \(repositoryData?.language ?? "")"
-        stargazersCountLabel.text = "\(repositoryData?.stargazersCount ?? 0) stars"
-        wachersCountLabel.text = "\(repositoryData?.watchersCount ?? 0) watchers"
-        forksCountLabel.text = "\(repositoryData?.forksCount ?? 0) forks"
-        openIssuesCountLabel.text = "\(repositoryData?.openIssuesCount ?? 0) open issues"
+        titleLabel.text = repositoryData.fullName
+        languageLabel.text = "Written in \(repositoryData.language)"
+        stargazersCountLabel.text = "\(repositoryData.stargazersCount) stars"
+        wachersCountLabel.text = "\(repositoryData.watchersCount) watchers"
+        forksCountLabel.text = "\(repositoryData.forksCount) forks"
+        openIssuesCountLabel.text = "\(repositoryData.openIssuesCount) open issues"
         
         setUpAvatarImageView()
     }
     
     private func setUpAvatarImageView() {
-        guard let repositoryData = repositoryData else {
-            return
-        }
-        
         guard let imgURL = URL(string: repositoryData.owner.avatarUrl) else {
             return
         }
