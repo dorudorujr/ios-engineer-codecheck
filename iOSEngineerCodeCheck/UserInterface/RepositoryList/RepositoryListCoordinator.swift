38fe3acc62700e246a3ee5b6f3f99372366f2ebd
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-21 20:04:30 +0900

    RepositoryListCoordinatorを使用して正常に遷移できるように中身を実装

--- iOSEngineerCodeCheck/UserInterface/RepositoryList/RepositoryListCoordinator.swift
+++ iOSEngineerCodeCheck/UserInterface/RepositoryList/RepositoryListCoordinator.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2022 YUMEMI Inc. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RepositoryListCoordinator: Coordinator {
+    private weak var viewController: RepositoryListViewController!
+    private weak var repositoryDetailCoordinator: RepositoryDetailCoordinatyor?
+    
+    init() {}
+    
+    func start(with parent: UIViewController) {
+        let vc = StoryboardScene.RepositoryList.initialScene.instantiate(with: (
+            repository: .init(),
+            coordinator: self
+        ))
+        let nc = UINavigationController(rootViewController: vc)
+        nc.modalPresentationStyle = .fullScreen
+        parent.present(nc, animated: false)
+        viewController = vc
+    }
+}
+
+extension RepositoryListCoordinator: RepositoryListCoordinatorDelegate {
+    func showDetail(with repositoryData: GitHubRepositoryData) {
+        let coordinator = RepositoryDetailCoordinatyor(repositoryData: repositoryData)
+        coordinator.start(with: viewController)
+        repositoryDetailCoordinator = coordinator
+    }
+}
