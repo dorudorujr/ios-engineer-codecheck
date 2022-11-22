fcf50c11b7e6f1cfc7554a3761865be4f6a39460
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-22 21:21:19 +0900

    API通信用のAction、RequestActionを追加

---
+++ iOSEngineerCodeCheck/Redux/Action/Common/RequestAction.swift
@@ -0,0 +1,15 @@
+//
+//  RequestAction.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 杉岡成哉 on 2022/11/22.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import ReSwift
+
+enum RequestAction<Request: Requestable>: Action {
+    case start(parameter: Request.Parameter)
+    case success(parameter: Request.Parameter, response: Request.Response)
+    case failure(parameter: Request.Parameter, error: Error)
+}
