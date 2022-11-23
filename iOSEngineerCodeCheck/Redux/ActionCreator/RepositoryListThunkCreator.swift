e4647942954178b87cb454eeacc6dfe3a4812c15
杉岡成哉 <a1209s518n928@gmail.com>
2022-11-23 12:07:54 +0900

    リポジトリ一覧画面のThunkCreatorを作成

---
+++ iOSEngineerCodeCheck/Redux/ActionCreator/RepositoryListThunkCreator.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryListThunkCreator.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 杉岡成哉 on 2022/11/23.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+typealias RepositoryListThunkCreator = RequestThunkCreator<AnySearchGitHubRepositoryRequest>
