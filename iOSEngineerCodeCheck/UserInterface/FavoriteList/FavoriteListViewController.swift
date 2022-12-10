77bcead749d219031d8c932f44f81540d3c8b18e
杉岡成哉 <a1209s518n928@gmail.com>
2022-12-10 19:56:02 +0900

    FavoriteListViewController追加

---
+++ iOSEngineerCodeCheck/UserInterface/FavoriteList/FavoriteListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteListViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 杉岡成哉 on 2022/12/10.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
