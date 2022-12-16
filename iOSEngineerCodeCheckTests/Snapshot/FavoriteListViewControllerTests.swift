aaaf38dd543552e9ace11d652e49c8324033bf0d
杉岡成哉 <a1209s518n928@gmail.com>
2022-12-17 00:03:06 +0900

    FavoriteListViewControllerTests追加

---
+++ iOSEngineerCodeCheckTests/Snapshot/FavoriteListViewControllerTests.swift
@@ -0,0 +1,52 @@
+//
+//  FavoriteListViewControllerTests.swift
+//  iOSEngineerCodeCheckTests
+//
+//  Created by 杉岡成哉 on 2022/12/16.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+@testable import iOSEngineerCodeCheck
+import XCTest
+import iOSSnapshotTestCase
+
+class FavoriteListViewControllerTests: FBSnapshotTestCase {
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+        fileNameOptions = [.screenSize, .screenScale]
+    }
+    
+    func test_exists() {
+        let vc = StoryboardScene.FavoriteList.initialScene.instantiate(with: (store: .init(state: .init(repositorys: repositorys)),
+                                                                              coordinator: FavoriteListCoordinator(),
+                                                                              favoriteThunkCreator: .init(repository: FavoriteRepositoryDataRepositoryImpl())))
+        verify(vc: vc)
+    }
+    
+    func test_empty() {
+        let vc = StoryboardScene.FavoriteList.initialScene.instantiate(with: (store: .init(state: nil),
+                                                                              coordinator: FavoriteListCoordinator(),
+                                                                              favoriteThunkCreator: .init(repository: FavoriteRepositoryDataRepositoryImpl())))
+        verify(vc: vc)
+    }
+    
+    private let repositorys: [GitHubRepositoryData] = [
+        .init(id: 1,
+              fullName: "apple/swift",
+              owner: .init(avatarUrl: "url"),
+              stargazersCount: 61196,
+              watchersCount: 61196,
+              language: "C++",
+              forksCount: 9836,
+              openIssuesCount: 6243),
+        .init(id: 2,
+              fullName: "tensorflow/swift",
+              owner: .init(avatarUrl: "url"),
+              stargazersCount: 6068,
+              watchersCount: 6068,
+              language: "Jupyter Notebook",
+              forksCount: 623,
+              openIssuesCount: 37)
+    ]
+}
