2e64d23591d05cc587285bac86634a8d96d835fd
杉岡成哉 <a1209s518n928@gmail.com>
2022-12-24 11:59:09 +0900

    CardViewとぼかし背景用のBlurEffectViewを追加

---
+++ iOSEngineerCodeCheck/UIComponent/BlurEffectView/BlurEffectView.swift
@@ -0,0 +1,44 @@
+//
+//  BlurEffectView.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 杉岡成哉 on 2022/12/24.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class BlurEffectView: UIView {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    private var blurEffectView: UIVisualEffectView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        nibInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        nibInit()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        blurEffectView?.frame = self.frame
+    }
+
+    private func nibInit() {
+        let nib = UINib(nibName: "BlurEffectView", bundle: nil)
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
+        addSubview(view)
+        
+        let blurEffect = UIBlurEffect(style: .regular)
+        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        guard let blurEffectView = blurEffectView else {
+            return
+        }
+        addSubview(blurEffectView)
+    }
+}
